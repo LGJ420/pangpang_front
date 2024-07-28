07f9122528bab7e695c67d3043d95d2f6188ffd5
LGJ420 <leegj420@gmail.com>
2024-07-28 12:36:24 +0900

    레이아웃 사이즈 미세조정

--- src/layouts/FooterLayout.js
+++ src/layouts/FooterLayout.js
@@ -1,7 +1,7 @@
 const FooterLayout = () => {
 
     return (
-        <footer className="h-[300px] bg-neutral-800 text-stone-300">
+        <footer className="h-[300px] min-w-[1350px] bg-neutral-800 text-stone-300">
             <div className="w-[1350px] m-auto">
                 응애 나 아기 푸터
             </div>
--- src/pages/cash/CashWalletPage.js
+++ src/pages/cash/CashWalletPage.js
@@ -1,8 +1,8 @@
 const CashWalletPage = () => {
 
     return (
-        <section className="flex flex-col items-center justify-center">
-            <img src="/images/cash.png" className="w-72"></img>
+        <section className="flex flex-col items-center justify-center h-screen">
+            <img src="/images/cash.png" className="w-52"></img>
 
             <div>
                 현재 나의 잔고
