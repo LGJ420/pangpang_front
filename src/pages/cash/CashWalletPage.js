2f996797ea5ba3de54a6aa1dbc619f9f015490a0
LGJ420 <leegj420@gmail.com>
2024-07-28 12:49:14 +0900

    골드 충전소 디자인 제작

--- src/pages/cash/CashWalletPage.js
+++ src/pages/cash/CashWalletPage.js
@@ -1,13 +1,10 @@
+import CartWalletComponent from "../../components/CartWalletComponent";
+
 const CashWalletPage = () => {
 
     return (
-        <section className="flex flex-col items-center justify-center h-screen">
-            <img src="/images/cash.png" className="w-52"></img>
-
-            <div>
-                현재 나의 잔고
-            </div>
-        </section>
+        
+        <CartWalletComponent />
     );
 }
 
