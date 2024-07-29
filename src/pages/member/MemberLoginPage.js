0bf219b48f8afebff9167adc705110f6409267f3
NSB(green) <suh240505@gmail.com>
2024-07-29 11:32:17 +0900

    로그인 페이지 css 수정

--- src/pages/member/MemberLoginPage.js
+++ src/pages/member/MemberLoginPage.js
@@ -1,50 +1,52 @@
 import { Link } from "react-router-dom";
-import BasicWidthLimitLayout from "../../layouts/BasicWidthLimitLayout";
 
 const MemberLoginPage = () => {
 
     return (
-        <BasicWidthLimitLayout>
-
-            <section className="h-full flex flex-col items-center justify-center">
-
-                {/* 로그인 페이지 */}
-                <h1 class="text-3xl">
-                    <span>
-                        파이널판타지14
-                        <br></br>
-                        <strong>로그인</strong>
-                    </span>
-                </h1>
+            <section className="login">
 
                 {/* 로그인 폼 */}
                 <form action="로그인url넣기" method="post">
-                    <div>
-                        <label for="member_ID"></label>
-                        <input name="member_ID" id="member_ID" placeholder="아이디를 입력해주세요."></input>
-                    </div>
-                    <div>
-                        <label for="password"></label>
-                        <input name="password" id="password" placeholder="비밀번호"></input>
-                    </div>
-                <button type="submit">로그인</button>
-                </form>
 
-                {/* 부가 서비스 */}
-                <div>
-                    <Link to={'/signup'} className="px-3">
-                        회원가입
-                    </Link>
-                    <Link to={'아이디/비밀번호 라우터 넣기'} className="px-3">
-                        아이디/비밀번호 찾기
-                    </Link>
-                    <Link to={'/'} className="px-3">
-                        홈으로 가기
-                    </Link>
-                </div>
+                    {/* 로그인 페이지 */}
+                    <h1>
+                        <span>
+                            팡이널팡타지14
+                            <br></br>
+                            <strong>로그인</strong>
+                        </span>
+                    </h1>
+
+                    <hr></hr>
+
+                    {/* 아이디/비밀번호 입력칸 */}
+                    <div>
+                        <div>
+                            <input name="memberId" id="memberId" placeholder="아이디를 입력해주세요."></input>
+                        </div>
+                        <div>
+                            <input name="memberPw" id="memberPw" type="password" placeholder="비밀번호를 입력해주세요."></input>
+                        </div>
+                    </div>
+                    <button type="submit">로그인</button>
+
+                    <hr></hr>
+
+                    {/* 링크 이동 */}
+                    <div className="">
+                        <Link to={'/signup'} className="px-5">
+                            회원가입
+                        </Link>
+                        <Link to={'아이디/비밀번호 라우터 넣기'} className="px-5">
+                            아이디/비밀번호 찾기
+                        </Link>
+                        <Link to={'/'} className="px-5">
+                            홈으로 가기
+                        </Link>
+                    </div>
+                </form>
             </section>
 
-        </BasicWidthLimitLayout>
     );
 }
 
