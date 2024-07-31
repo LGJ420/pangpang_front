c35906e6d33a8022c7844b3b3f6580f3d44ef7b8
NSB(green) <suh240505@gmail.com>
2024-07-31 11:53:32 +0900

    아이디, 비밀번호 찾기 프론트 코드 작성

--- src/pages/member/MemberFindPage.js
+++ src/pages/member/MemberFindPage.js
@@ -7,109 +7,220 @@ import {
     FormErrorMessage,
     FormHelperText,
     } from '@chakra-ui/react'
+import { useState } from "react";
+import axios from 'axios';
 
 const MemberLoginPage = () => {
 
-    const navigate = useNavigate();
+    // ☆★☆★☆★☆★ 아이디 ☆★☆★☆★☆★
 
+    // 아이디 찾기 관련 state, onchange 메소드
+    const [memberNameInFindId, setMemberNameInFindId] = useState('');
+    const [memberBirthInFindId, setMemberBirthInFindId] = useState('');
+    
+    const handleMemberNameInFindId = (e)=>{
+        setMemberNameInFindId(e.target.value);
+    }
+    const handleMemberBirthInFindId = (e)=>{
+        // 생년월일에 숫자만 허용하는 정규식 사용
+        const validInputValue = e.target.value.replace(/[^0-9]/g, "");
+        setMemberBirthInFindId(validInputValue);
+    }
+    
+    const navigate = useNavigate();
+    
     // 아이디 찾기 버튼
     const handleFindId = () => {
         
-        // 아이디 찾는 조건 작성
-        // ex. 이름, 생년월일 적혀있는가?
-
-        // post로 이름, 생년월일 제출 함수 작성
+        // 1. 제대로 입력되었는지 확인
+        console.log("click find_id");
+        console.log("이름 : " + memberNameInFindId);
+        console.log("생년월일 : " + memberBirthInFindId);
         
-        // 제출한 이름, 생년월일이 있으면 아래 링크로 이동
-        navigate("/find_id");
+        // 2. 아이디 찾는 조건 작성
+        //    2-1. input에 빠짐없이 다 적혀있는가?
+        if([memberNameInFindId, memberBirthInFindId].includes('')){
+            const errorMsg = "입력하지 않은 사항이 있습니다.";
+            console.error(errorMsg)
+            alert(errorMsg);
 
-        // 없으면 이름, 생년월일 재작성 유도
+            return;
+        }
 
+        //    2-2. 생년월일이 6자리로 적혀있는가?
+        if (memberBirthInFindId.length !== 6){
+            alert("생년월일을 6자리 숫자로 입력해주세요.")
+        }
+        
+        // 3. post로 이름, 생년월일 제출 함수 작성
+        axios
+        .post("http://localhost:8080/api/member/find",{
+                memberNameInFindId : memberNameInFindId,
+                memberBirthInFindId : memberBirthInFindId
+            })
+            
+            // 3-1. 제출한 이름, 생년월일이 있으면 아래 링크로 이동
+            .then((response)=>{
+                console.log(response)
+                navigate("/find_id");
+            })
+            
+            // 3-2. 없으면 에러 발생
+            .catch((error)=>{
+                    console.error("회원가입 요청 중 오류 발생", error);
+            });
+    }
+
+    // =======================================================================
+
+    // ☆★☆★☆★☆★ 비밀번호 ☆★☆★☆★☆★
+    // 비밀번호 찾기 관련 state, onchange 메소드
+    const [memberIdInFindPw, setMemberIdInFindPw] = useState('');
+    const [memberNameInFindPw, setMemberNameInFindPw] = useState('');
+    const [memberBirthInFindPw, setMemberBirthInFindPw] = useState('');
+    
+    const handleMemberIdInFindPw = (e)=>{
+        setMemberIdInFindPw(e.target.value);
+    }
+    const handleMemberNameInFindPw = (e)=>{
+        setMemberNameInFindPw(e.target.value);
+    }
+    const handleMemberBirthInFindPw = (e)=>{
+        // 생년월일에 숫자만 허용하는 정규식 사용
+        const validInputValue = e.target.value.replace(/[^0-9]/g, "");
+        setMemberBirthInFindPw(validInputValue);
     }
 
     // 비밀번호 찾기 버튼
     const handleFindPw = () => {
         
-        // 비밀번호 찾는 조건 작성
-        // ex. 아이디, 이름, 생년월일 적혀있는가?
-
-        // post로 아이디, 이름, 생년월일 제출 함수 작성
+        // 1. 제대로 입력되었는지 확인
+        console.log("click find_pw");
+        console.log("ID : " + memberIdInFindPw);
+        console.log("이름 : " + memberNameInFindPw);
+        console.log("생년월일 : " + memberBirthInFindPw);
         
-        // 제출한 아이디, 이름, 생년월일이 있으면 아래 링크로 이동
-        navigate("/find_pw");
+        // 2. 비밀번호 찾는 조건 작성
+        //    2-1. input에 빠짐없이 다 적혀있는가?
+        if([memberIdInFindPw, memberNameInFindPw, memberBirthInFindPw].includes('')){
+            const errorMsg = "입력하지 않은 사항이 있습니다.";
+            console.error(errorMsg)
+            alert(errorMsg);
 
-        // 없으면 이름, 생년월일 재작성 유도
+            return;
+        }
+
+        //    2-2. 생년월일이 6자리로 적혀있는가?
+        if (memberBirthInFindId.length !== 6){
+            alert("생년월일을 6자리 숫자로 입력해주세요.")
+        }
+        
+        // 3. post로 아이디, 이름, 생년월일 제출 함수 작성
+        axios
+        .post("http://localhost:8080/api/member/find",{
+                memberIdInFindPw : memberNameInFindPw,
+                memberNameInFindPw : memberNameInFindPw,
+                memberBirthInFindPw : memberBirthInFindPw
+            })
+            
+            // 3-1. 제출한 아이디, 이름, 생년월일이 있으면 아래 링크로 이동
+            .then((response)=>{
+                console.log(response)
+                navigate("/find_pw");
+            })
+            
+            // 3-2. 없으면 에러 발생
+            .catch((error)=>{
+                    console.error("회원가입 요청 중 오류 발생", error);
+            });
     }
-
+                
     return (
-            <section className="account_management">
+        <section className="account_management">
 
-                {/* 아이디/비밀번호 찾기 페이지 */}
-                <h1>
-                    <span>
-                    팡팡게임즈
-                        <br></br>
-                        <strong>아이디, 비밀번호 찾기</strong>
-                    </span>
-                    <hr></hr>
-                </h1>
+            {/* 아이디/비밀번호 찾기 페이지 */}
+            <h1>
+                <span>
+                팡팡게임즈
+                    <br></br>
+                    <strong>아이디, 비밀번호 찾기</strong>
+                </span>
+                <hr></hr>
+            </h1>
 
 
-                {/* 아이디, 비밀번호 찾기 컨테이너 */}
-                <div className="form_container">
+            {/* 아이디, 비밀번호 찾기 컨테이너 */}
+            <div className="form_container">
 
-                    {/* 아이디 찾기 */}
+                {/* 아이디 찾기 */}
+                <div>
+                    
                     <div>
-                        
-                        <div>
-                            <FormControl isRequired>
-                                <FormLabel>이름</FormLabel>
-                                <Input placeholder='이름을 입력해주세요.' />
-                            </FormControl>
+                        <FormControl isRequired>
+                            <FormLabel>이름</FormLabel>
+                            <Input
+                            value={memberNameInFindId} 
+                            onChange={handleMemberNameInFindId}
+                            placeholder='이름을 입력해주세요.' />
+                        </FormControl>
 
-                            <FormControl isRequired>
-                                <FormLabel>생년월일</FormLabel>
-                                <Input placeholder='ex.881225' />
-                            </FormControl>
-                        </div>
-
-                        <button 
-                        onClick={handleFindId}
-                        className="button">
-                            아이디 찾기
-                        </button>
-
+                        <FormControl isRequired>
+                            <FormLabel>생년월일</FormLabel>
+                            <Input 
+                            value={memberBirthInFindId}
+                            onChange={handleMemberBirthInFindId}
+                            placeholder='ex.881225' />
+                        </FormControl>
                     </div>
 
-                    {/* 비밀번호 찾기 */}
-                    <div>
-
-                        <div>
-                            <FormControl isRequired>
-                                <FormLabel>아이디</FormLabel>
-                                <Input placeholder='아이디를 입력해주세요.' />
-                            </FormControl>
-                            <FormControl isRequired>
-                                <FormLabel>이름</FormLabel>
-                                <Input placeholder='이름을 입력해주세요.' />
-                            </FormControl>
-                            <FormControl isRequired>
-                                <FormLabel>생년월일</FormLabel>
-                                <Input placeholder='ex.881225' />
-                            </FormControl>
-                        </div>
-
-                        <button 
-                        onClick={handleFindPw}
-                        className="button">
-                            비밀번호 찾기
-                        </button>
-
-                    </div>
+                    <button 
+                    onClick={handleFindId}
+                    className="button">
+                        아이디 찾기
+                    </button>
 
                 </div>
 
-            </section>
+                {/* 비밀번호 찾기 */}
+                <div>
+
+                    <div>
+                        <FormControl isRequired>
+                            <FormLabel>아이디</FormLabel>
+                            <Input 
+                            value={memberIdInFindPw}
+                            onChange={handleMemberIdInFindPw}
+                            placeholder='아이디를 입력해주세요.' />
+                        </FormControl>
+
+                        <FormControl isRequired>
+                            <FormLabel>이름</FormLabel>
+                            <Input 
+                            value={memberNameInFindPw}
+                            onChange={handleMemberNameInFindPw}
+                            placeholder='이름을 입력해주세요.' />
+                        </FormControl>
+
+                        <FormControl isRequired>
+                            <FormLabel>생년월일</FormLabel>
+                            <Input 
+                            value={memberBirthInFindPw}
+                            onChange={handleMemberBirthInFindPw}
+                            placeholder='ex.881225' />
+                        </FormControl>
+                    </div>
+
+                    <button 
+                    onClick={handleFindPw}
+                    className="button">
+                        비밀번호 찾기
+                    </button>
+
+                </div>
+
+            </div>
+
+        </section>
 
     );
 }
