557bdc9f23530e1ebe476fa6b29b42df1cb4439b
LGJ420 <leegj420@gmail.com>
2024-07-31 21:26:49 +0900

    리코일 라이브러리 적용

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider>
-        <App />  
+        <RecoilRoot>
+            <App />  
+        </RecoilRoot>
     </ChakraProvider>
 );   
 
