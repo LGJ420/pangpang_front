99d177a04507e9b8b738f0c07c2236dfab8a4e3f
LGJ420 <LGJ420@gmail.com>
2024-08-02 18:01:36 +0900

    장바구니 아이템목록 삭제 api 작성

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -20,12 +20,19 @@ export const postCartAdd = async (cartObj) => {
     return res.data;
 }
 
-// 지금은 사용자가 1번 고정 나중에 로그인한 사용자로 수정할 예정
-export const deleteCartOne = async (cartListObj) => {
 
-    console.log(cartListObj);
-    const res = await axios.delete(`${prefix}`, { data: cartListObj });
+// 지금은 사용자가 1번 고정 나중에 로그인한 사용자로 수정할 예정
+export const deleteCartOne = async (cartObj) => {
+
+    const res = await axios.delete(`${prefix}`, { data: cartObj });
 
     return res.data;
 
+}
+
+export const deleteCarts = async (dtoList) => {
+
+    const res = await axios.delete(`${prefix}`, { data: dtoList });
+
+    return res.data;
 }
