00c397a32c3eb46b765e0e16c3929d0c999f3c41
LGJ420 <leegj420@gmail.com>
2024-08-03 10:44:25 +0900

    index.html 변경, 배너 카로우셀 적용

--- src/components/BannerComponent.js
+++ src/components/BannerComponent.js
@@ -1,30 +1,41 @@
+import { useState } from "react";
+import { Carousel } from "react-bootstrap";
+
 const BannerComponent = () => {
+
+
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
 
     return (
 
         <section>
-            <div className="flex relative h-[440px]">
-                <div className="absolute inset-0"
-                        style={{
-                            backgroundImage: "url('/images/main1.jpg')",
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                            opacity: 0.9,
-                            zIndex: -1
-                        }}>
-                </div>
-                <div className="w-[1350px] m-auto">
-                    <div className="text-white">
-                        <h2 className="text-6xl">
-                            우대 서버만의 특별한 혜택!
-                        </h2>
-                        <p className="mt-5">
-                            빠른 성장과 기초 아이템 지원 혜택까지!<br/>
-                            우대 서버에서만 누릴 수 있는 혜택을 확인해보세요!
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item className="max-h-[400px]">
+                    <img src="images/main1.jpg" text="main1" />
+                    <Carousel.Caption className="h-2/3 pl-16">
+                    <h3 className="text-left text-4xl font-bold">팡팡 게임즈의 특별 혜택</h3>
+                    <p className="text-left">팡팡게임즈에서는 여러가지 게임상품을<br />특별 할인 가격으로 구매가 가능합니다!</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="max-h-[400px]">
+                    <img src="images/main2.jpg" text="main2" />
+                    <Carousel.Caption className="h-2/3 pl-16">
+                    <h3 className="text-left text-4xl font-bold">커뮤니티 오픈</h3>
+                    <p className="text-left">자유게시판이 오픈하였습니다.<br />지금 바로 여러 유저들과 소통해보세요!</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className="max-h-[400px]">
+                    <img src="images/main3.jpg" text="main3" />
+                    <Carousel.Caption className="h-2/3 pl-16">
+                    <h3 className="text-left text-4xl font-bold">무료 플레이 이벤트</h3>
+                    <p className="text-left">기간 제한 없이 70레벨까지!<br />무료 플레이 혜택과 함께 최고의 모험을 즐겨보세요.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </section>
 
     );
