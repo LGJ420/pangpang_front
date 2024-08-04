39cd0a4c92a1a73b3ecc26e81ba73dc7192bee88
LGJ420 <leegj420@gmail.com>
2024-08-04 17:41:15 +0900

    더미 게임 삽입

--- src/pages/game/GameIndexPage.js
+++ src/pages/game/GameIndexPage.js
@@ -1,15 +1,19 @@
-import BasicWidthLimitLayout from "../../layouts/BasicWidthLimitLayout";
+import BasicNoHeightLayout from "../../layouts/BasicNoHeightLayout";
 
 const GameIndexPage = () => {
 
     return (
-        <BasicWidthLimitLayout>
+        <BasicNoHeightLayout>
 
-        <section>
-            여긴 게임페이지다냥
-        </section>
-        
-        </BasicWidthLimitLayout>
+            <iframe src="https://plicy.net/GameEmbed/181045"
+                className="w-[1350px] h-[700px]"/>
+
+            <div className="flex flex-col items-center justify-center p-3">
+                <h3>조작키</h3>
+                <p>이동 : WASD<br/>공격 : ←↑↓→</p>
+            </div>
+
+        </BasicNoHeightLayout>
     );
 }
 
