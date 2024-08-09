ac15f0e6294dfdf9547d654152d8c4e0fd046ef7
LGJ420 <leegj420@gmail.com>
2024-08-09 10:40:03 +0900

    api에 토큰 적용

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -4,10 +4,16 @@ export const API_SERVER_HOST = 'http://localhost:8080';
 
 const prefix = `${API_SERVER_HOST}/api/cart`;
 
+const token = localStorage.getItem('token');
+
 
 export const getCartList = async () => {
     
-    const res = await axios.get(`${prefix}`);
+    const res = await axios.get(`${prefix}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
     
     return res.data;
 }
@@ -15,24 +21,38 @@ export const getCartList = async () => {
 
 export const postCartAdd = async (cartObj) => {
     
-    const res = await axios.post(`${prefix}`, cartObj);
+    const res = await axios.post(`${prefix}`, cartObj, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
 
     return res.data;
 }
 
-
-// 지금은 사용자가 1번 고정 나중에 로그인한 사용자로 수정할 예정
-export const deleteCartOne = async (cartObj) => {
-
-    const res = await axios.delete(`${prefix}`, { data: cartObj });
-
-    return res.data;
-
-}
 
 export const putCartOne = async (cartObj) => {
-
-    const res = await axios.put(`${prefix}`, cartObj);
+    
+    const res = await axios.put(`${prefix}`, cartObj, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
 
     return res.data;
+}
+
+
+// 삭제는 설정칸에 data로 넣어줘야한다
+export const deleteCartOne = async (cartObj) => {
+
+    const res = await axios.delete(`${prefix}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        data: cartObj
+    });
+
+    return res.data;
+
 }
