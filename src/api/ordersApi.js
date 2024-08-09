00faf6fd2156fbf04400190ce7fa54c77d8a9950
LGJ420 <leegj420@gmail.com>
2024-08-09 14:43:38 +0900

    token값 불러오기를 메서드 안으로 이동

--- src/api/ordersApi.js
+++ src/api/ordersApi.js
@@ -4,11 +4,12 @@ export const API_SERVER_HOST = 'http://localhost:8080';
 
 const prefix = `${API_SERVER_HOST}/api/orders`;
 
-const token = localStorage.getItem("token");
 
 
 export const getOrdersList = async (search) => {
     
+    const token = localStorage.getItem("token");
+
     const res = await axios.get(`${prefix}/list`, {
         headers: {
             'Authorization': `Bearer ${token}`
@@ -20,7 +21,10 @@ export const getOrdersList = async (search) => {
 }
 
 
+
 export const postOrdersAdd = async (ordersObj) => {
+
+    const token = localStorage.getItem("token");
     
     const res = await axios.post(`${prefix}`, ordersObj, {
         headers: {
