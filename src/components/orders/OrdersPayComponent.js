9a1998d4458ab8ba27c8e07119dd5cdc586d7485
LGJ420 <leegj420@gmail.com>
2024-08-11 02:13:35 +0900

    오타 수정

--- src/components/orders/OrdersPayComponent.js
+++ src/components/orders/OrdersPayComponent.js
@@ -74,7 +74,7 @@ const OrdersPayComponent = () => {
 
         return () => {
 
-            document.body.removeChild(script);
+            document.head.removeChild(script);
         };
 
     }, []);
