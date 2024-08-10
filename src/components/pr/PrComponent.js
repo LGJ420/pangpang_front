cff3a5049ece6cda7a00dc66d57c5c815e15d69b
LGJ420 <leegj420@gmail.com>
2024-08-10 22:42:17 +0900

    게임소개 초기화, 틀 제작

--- src/components/pr/PrComponent.js
+++ src/components/pr/PrComponent.js
@@ -1,58 +1,72 @@
+import React, { useEffect } from 'react';
+import '../../css/prComponent.css';
+
 const PrComponent = () => {
 
-    return (
+    useEffect(() => {
 
-        <section className="bg-black text-white">
-            <div className="flex flex-col items-center">
-                <div className="relative">
-                    <img src="/images/pr_bg1.jpg"></img>
-                    <p className="absolute top-1/3 -translate-y-1/2 w-full text-center text-2xl leading-relaxed text-zinc-300 overflow-hidden">
-파이널판타지14는 2015년 &lt;신생 에오르제아&gt; 를 시작으로<br />
-&lt;창천의 이슈가르드&gt;, &lt;홍련의 해방자&gt;, &lt;칠흑의 반역자&gt;에 이어<br />
-네번째 확장팩 &lt;효월의 종언&gt;을 선보인 정통 온라인 MMORPG입니다.<br />
-<br />
-신규 확장팩 &lt;효월의 종언&gt;은 별과 생명을 둘러싼 대서사시 '하이델린・조디아크편'이 완결되는 마지막 확장팩으로써<br />
-극적인 스토리 전개와 높은 긴장감을 선사하며 ‘몰입도 높은 최고 수준의 서사’라는 평가와 함께<br />
-세계 최대 평론 사이트인 메타크리틱에서 92점이라는 높은 평점을 기록하여<br />
-확장팩 &lt;칠흑의 반역자&gt;에 이어 2연속으로 반드시 플레이해볼 것을 권장하는 ‘MUST-PLAY’ 작품으로 선정되었습니다.<br />
-<br />
-비디오 게임에서 가장 많은 오리지널 사운드 트랙을 가진 게임으로 기네스 기록을 달성하기도 한 파이널판타지14는<br />
-웅장한 오케스트라 사운드부터 율동적인 록 사운드까지 게임 OST 이상의 수준 높은 감동을 선사합니다.<br />
-완벽한 OST와 초호화 성우진이 만들어 낸 완벽한 더빙, 스토리에 따라 이어지는 시네마틱 영상과 환상적인 그래픽을 통하여<br />
-실제로 판타지 세계에 있는듯한 완벽한 몰입감을 제공합니다.<br />
-<br />
-파이널판타지14는 여러분께 환상적인 판타지의 세계에서 경험할 수 있는 최고의 모험을 선물할 것입니다.
-                    </p>
-                </div>
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('primage-animation');
+            }
+            });
+        });
 
+        const primages = document.querySelectorAll('.primage');
+        primages.forEach((primage) => observer.observe(primage));
 
+        return () => {
+            primages.forEach((primage) => observer.unobserve(primage));
+        };
 
-                <p className="mb-32 w-full text-center text-2xl leading-relaxed text-zinc-300 overflow-hidden">
-8개의 종족과 각 2개의 부족, 총 16개의 캐릭터로 다양한 커스터마이징이 가능합니다.<br />
-<br />
-당신만의 캐릭터로 파이널판타지14에서 모험을 시작해보세요!
-                </p>
+    }, []);
 
+  return (
+    <div id="prComponent">
+      <section>
+        <div className='w-[1350px] m-auto'>
+            <img className='primage w-[50rem]' src='/images/pr_mario.png' />
+            <h3>슈퍼마리오 브라더스</h3>
+            <p>
+    《슈퍼 마리오브라더스》[a]는 닌텐도가 개발하고 배급한 패밀리 컴퓨터용 플랫폼 게임이다. 1983년 아케이드용으로 출시된 《마리오브라더스》의 후속작이자 이후 탄생한 《슈퍼 마리오》 시리즈의 첫 번째 작품이다. 1985년 9월 13일 일본에서 처음 발매됐으며 북미 지역에선 1985년 10월 혹은 11월 중에 출시됐다. 1986년에 전세계 보급 아케이드 시스템 닌텐도 VS. 시스템으로 이식됐으며 1987년에 유럽 지역에 출시됐다.
 
-                <div className="relative">
-                    <img src="/images/pr_bg2.jpg"></img>
-                    <p className="absolute top-1/3 -translate-y-1/2 w-full text-center text-2xl leading-relaxed text-zinc-300 overflow-hidden">
-예측을 뒤엎는 세계관 속에 새롭게 펼쳐지는 대서사시<br />
-제7재해로 폐허가 된 에오르제아의 5년 후의 세계로 날아간 모험가는<br />
-폐허를 딛고 새로운 희망을 품은 땅 “에오르제아”에서 새로운 모험을 시작하게 됩니다.<br />
-모험가는 에오르제아의 울다하, 그리다니아, 림사 로민사 3개의 도시 국가를 넘어<br />
-북방의 성도 이슈가르드, 먼 동쪽 오사드 소대륙까지 대륙을 종횡무진으로 활동하며<br />
-세계를 지키려는 많은 사람과 만나 수많은 위협들에 맞서며 끊임없이 성장합니다.<br />
-<br />
-그리고 지금,<br />
-에오르제아를 넘어 새로운 세계에서도 세계 멸망의 위험에서 구원해줄 모험가를 기다리고 있습니다.
-                    </p>
+    플레이어는 주인공 마리오, 혹은 2인용으로 시작할 시 루이지를 조종하며 악당 쿠파가 이끄는 군단이 점령한 버섯 왕국을 모험해 피치 공주를 구출하는 것이 게임의 주목표이다. 게임 내 진행은 횡스크롤 형태의 스테이지 내에서 위험요소와 적들을 제거하거나 돌파하는 형식으로 이뤄지며, 때때로 마리오를 크게 하는 슈퍼버섯, 불꽃을 발사하게 하는 파이어플라워, 일시적으로 플레이어를 무적으로 만드는 스타같은 파워업을 획득해 이용할 수 있다.
 
-                </div>
+    《슈퍼 마리오브라더스》는 미야모토 시게루와 데즈카 다카시가 공동으로 기획하고 개발한 작품으로, 그들이 패밀리 컴퓨터 출시 후 《데빌 월드》, 《익사이트바이크》와 《스파르탄 X》같이 3년간 참여한 게임들에서 얻은 경험에서 착안해 제작한 게임이다. 닌텐도가 이전에 발매한 《동키콩》과 《마리오브라더스》의 '플랫폼 운동 게임' 규칙을 개량하여 《슈퍼 마리오브라더스》에 접목해, 플랫폼 게임플레이의 튜토리얼 역할을 하는 첫 번째 스테이지 월드 1-1같은 요소들을 도입했다.
 
-            </div>
-        </section>
-    );
-}
+    《슈퍼 마리오브라더스》은 발매 당시 정확한 조종감과 단순하면서도 능숙한 기술을 필요로 하는 깊은 게임플레이로 전폭적인 인기를 끌었다. 원작 패밀리 컴퓨터판은 전세계 판매량이 4024만 장을 기록해 상업적인 대성공을 거뒀다. 패밀리 컴퓨터와 함께 1983년 북미 콘솔 비디오 게임 시장의 위기 이후 비디오 게임 산업에 재활력을 불어넣은 중요 요인으로 꼽힌다. 마리오와 《슈퍼 마리오브라더스》는 대중문화의 선두주자가 됐다.
+            </p>
+        </div>
+      </section>
+      <section>
+        <div className='w-[1350px] m-auto'>
+            <img className='primage w-[50rem]' src='/images/pr_kirby.png' />
+            <h3>별의 요정 커비</h3>
+            <p>
+            </p>
+        </div>
+      </section>
+      <section>
+        <div className='w-[1350px] m-auto'>
+            <img className='primage w-[50rem]' src='/images/pr_zelda.png' />
+            <h3>젤다의 전설</h3>
+        </div>
+      </section>
+      <section>
+        <div className='w-[1350px] m-auto'>
+            <img className='primage w-[50rem]' src='/images/pr_pokemon.png' />
+            <h3>포켓몬스터</h3>
+        </div>
+      </section>
+      <section>
+        <div className='w-[1350px] m-auto'>
+            <img className='primage w-[50rem]' src='/images/pr_animal.png' />
+            <h3>동물의 숲</h3>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default PrComponent;
