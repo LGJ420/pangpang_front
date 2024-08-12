46a9cbe796a9637a85a76c21d89a5e62a6048d57
NSB(green) <suh240505@gmail.com>
2024-08-12 18:07:06 +0900

    마이페이지 내정보변경 시작

---
+++ src/components/member/MemberProfileComponent.js
@@ -0,0 +1,13 @@
+const MemberProfileComponent = () => {
+    return(
+        <div>
+            <p>내 정보 수정</p>
+            <p>팡팡게임즈 대표 프로필과 닉네임을 수정 하실 수 있습니다.</p>
+
+            
+
+        </div>
+    );
+}
+
+export default MemberProfileComponent;
