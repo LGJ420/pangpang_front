2ec75358c6dac66240563b291f0e034b681e8332
NSB(green) <suh240505@gmail.com>
2024-08-13 15:35:54 +0900

    마이페이지 내정보변경에서 사용자의 비밀번호와 입력된 비밀번호가 같으면 사용자의 엔티티를 출력

--- src/components/member/MemberConfirmBeforeProfileComponent.js
+++ src/components/member/MemberConfirmBeforeProfileComponent.js
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import styles from '../../css/memberPage.module.css';
+import useCustomToken from './../../hooks/useCustomToken';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const MemberConfirmBeforeProfileComponent = () => {
 
+    // ⓐ 비밀번호  state
     const [memberPwInConfirmBeforeProfile, setMemberPwInConfirmBeforeProfile] = useState('');
 
+    // ⓑ input에 작성한 비밀번호 실시간으로 저장하는 핸들러(ⓐ이랑 짝꿍)
     const handleMemberPwInConfirmBeforeProfile = (e) => {
         setMemberPwInConfirmBeforeProfile(e.target.value);
     }
+
+    const navigate = useNavigate();
+    
+    // 토큰 가져오기(프린시펄 하려면 토큰을 보내야함)
+    // 로컬스토리지에 토큰을 가져옴
+    const token = localStorage.getItem("token");
     
     // 비밀번호 확인 버튼
     const clickConfirmBeforeProfile = () => {
@@ -24,7 +35,39 @@ const MemberConfirmBeforeProfileComponent = () => {
             return; 
         }
 
-        // 3. post로 비밀번호 전송
+        // 3. post로 토큰(아이디), 비밀번호 전송
+        axios
+        .post("http://localhost:8080/api/member/confirm_before_profile",
+            {memberPwInConfirmBeforeProfile : memberPwInConfirmBeforeProfile },
+            {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    }
+            })
+            
+            // 3-1. 토큰에 저장된 사용자의 비밀번호와 입력된 비밀번호가 같으면 아래 링크로 이동하면서 데이터 전달
+            .then((response)=>{
+                console.log("response 이후 콘솔 내용")
+                // ▼▼▼ 출력 제대로 되는지 확인용 ▼▼▼
+                console.log(response.data)
+
+                // // 페이지 이동(프로필변경 전 확인->프로필변경) 및 데이터 전달
+                navigate("/profile", {state : {data : response.data}});
+            })
+            
+            // 3-2. 없으면 에러 발생
+            .catch((error)=>{
+                const errorMsg = "비밀번호가 일치하지 않습니다.";
+                alert(errorMsg);
+                console.error(errorMsg);
+            });
+    }
+
+    /* 검색 인풋창 엔터키만 눌러도 검색 */
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            clickConfirmBeforeProfile();
+        }
     }
 
     return(
@@ -41,7 +84,8 @@ const MemberConfirmBeforeProfileComponent = () => {
                 <input 
                 type='password'
                 placeholder='비밀번호를 변경해주세요.' 
-                onChange={handleMemberPwInConfirmBeforeProfile}/>
+                onChange={handleMemberPwInConfirmBeforeProfile}
+                onKeyDown={handleKeyDown}/>
 
             </div>
             <button onClick={clickConfirmBeforeProfile}>확인</button>
