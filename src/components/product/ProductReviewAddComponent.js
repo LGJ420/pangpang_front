087e508b730505f09afd9e5840ebc8c4903a6b1c
LGJ420 <leegj420@gmail.com>
2024-08-15 01:12:59 +0900

    리뷰 등록시 리다이렉트

--- src/components/product/ProductReviewAddComponent.js
+++ src/components/product/ProductReviewAddComponent.js
@@ -73,36 +73,48 @@ const ProductReviewAddComponent = ({productId}) => {
     }
 
 
+
     const handleClickSubmit = () => {
 
+        // 여유가 되면 모달창을 제작해서 바꿀예정
+        // eslint-disable-next-line no-restricted-globals
+        const submit = confirm("리뷰를 등록하시겠습니까?");
+
+        if (!submit) {
+
+            return;
+        }
+        
         const formData = new FormData();
         formData.append("reviewContent", review.reviewContent);
         formData.append("rating", review.rating);
         formData.append("productId", review.productId);
-
+        
         if (uploadFile) {
             
             formData.append("reviewFile", uploadFile);
         }
-    
-        try {
 
-            postReviewAdd(formData);
-        }
-        catch (error) {
-
-            console.log(error);
-        }
+        postReviewAdd(formData).then(()=>{
+            alert("등록이 완료되었습니다");
+            navigate({ pathname: '/mypage/review' });
+        });
+        
     }
+    
+
+
 
     const handleChangeReview = (e) => {
-
+        
         review[e.target.name] = e.target.value;
-
+        
         setReview({...review});
     }
 
 
+
+    
     return (
 
         <section className="my-10">
@@ -123,43 +135,43 @@ const ProductReviewAddComponent = ({productId}) => {
                     <fieldset id="rate">
                         <input type="radio" id="rating10" name="rating" value="10"
                         onChange={handleChangeReview} />
-                        <label for="rating10" title="5점"></label>
+                        <label htmlFor="rating10" title="5점"></label>
                         
                         <input type="radio" id="rating9" name="rating" value="9" 
                         onChange={handleChangeReview} />
-                        <label class="half" for="rating9" title="4.5점"></label>
+                        <label className="half" htmlFor="rating9" title="4.5점"></label>
                         
                         <input type="radio" id="rating8" name="rating" value="8" 
                         onChange={handleChangeReview} />
-                        <label for="rating8" title="4점"></label>
+                        <label htmlFor="rating8" title="4점"></label>
                         
                         <input type="radio" id="rating7" name="rating" value="7" 
                         onChange={handleChangeReview} />
-                        <label class="half" for="rating7" title="3.5점"></label>
+                        <label className="half" htmlFor="rating7" title="3.5점"></label>
                         
                         <input type="radio" id="rating6" name="rating" value="6" 
                         onChange={handleChangeReview} />
-                        <label for="rating6" title="3점"></label>
+                        <label htmlFor="rating6" title="3점"></label>
                         
                         <input type="radio" id="rating5" name="rating" value="5" 
                         onChange={handleChangeReview} />
-                        <label class="half" for="rating5" title="2.5점"></label>
+                        <label className="half" htmlFor="rating5" title="2.5점"></label>
                         
                         <input type="radio" id="rating4" name="rating" value="4" 
                         onChange={handleChangeReview} />
-                        <label for="rating4" title="2점"></label>
+                        <label htmlFor="rating4" title="2점"></label>
                         
                         <input type="radio" id="rating3" name="rating" value="3" 
                         onChange={handleChangeReview} />
-                        <label class="half" for="rating3" title="1.5점"></label>
+                        <label className="half" htmlFor="rating3" title="1.5점"></label>
                         
                         <input type="radio" id="rating2" name="rating" value="2" 
                         onChange={handleChangeReview} />
-                        <label for="rating2" title="1점"></label>
+                        <label htmlFor="rating2" title="1점"></label>
                         
                         <input type="radio" id="rating1" name="rating" value="1" 
                         onChange={handleChangeReview} />
-                        <label class="half" for="rating1" title="0.5점"></label>
+                        <label className="half" htmlFor="rating1" title="0.5점"></label>
                     </fieldset>
                 </div>
 
