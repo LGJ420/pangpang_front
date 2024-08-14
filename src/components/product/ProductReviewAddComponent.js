b13d555ebd23c891af80312d831845335a3b6612
LGJ420 <LGJ420@gmail.com>
2024-08-14 18:00:32 +0900

    리뷰추가 form 연결중

--- src/components/product/ProductReviewAddComponent.js
+++ src/components/product/ProductReviewAddComponent.js
@@ -1,10 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../../css/productReviewAddComponent.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import { postReviewAdd } from "../../api/productReviewApi";
 
-const ProductReviewAddComponent = () => {
+const initReview = {
+    rating: 0,
+    reviewContent: "",
+    reviewFile: "",
+    productId: 0
+}
 
-    const [file, setFile] = useState(null);
+const ProductReviewAddComponent = ({productId}) => {
+
+    const [productData, setProductData] = useState([]);
+    const [uploadFile, setUploadFile] = useState(null);
     const [previewUrl, setPreviewUrl] = useState('');
+    const [review, setReview] = useState(initReview);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+
+    useEffect(() => {
+
+        try{
+            setProductData(location.state.dto);
+
+            review[productId] = location.state.dto.productId;
+
+            setReview({...review});
+        }
+        catch(e){
+            
+            console.log(e);
+        }
+
+    }, [location.state]);
 
 
     const handleFileChange = event => {
@@ -25,9 +56,9 @@ const ProductReviewAddComponent = () => {
             return;
         }
 
-        setFile(file);
+        setUploadFile(file);
         previewFile(file);
-    };
+    }
 
 
     const previewFile = file => {
@@ -36,7 +67,38 @@ const ProductReviewAddComponent = () => {
             setPreviewUrl(reader.result);
         };
         reader.readAsDataURL(file);
-    };
+    }
+
+
+    const handleClickSubmit = () => {
+
+        const formData = new FormData();
+        formData.append("reviewContent", review.reviewContent);
+        formData.append("rating", review.rating);
+        formData.append("productId", review.productId);
+
+        if (uploadFile) {
+            
+            formData.append("reviewFile", uploadFile);
+        }
+    
+        try {
+            console.log("머야이거" + review.productId);
+            console.log(location.state.dto);
+            // postReviewAdd(formData);
+        }
+        catch (error) {
+
+            console.log(error);
+        }
+    }
+
+    const handleChangeReview = (e) => {
+
+        review[e.target.name] = e.target.value;
+
+        setReview({...review});
+    }
 
 
     return (
@@ -49,7 +111,7 @@ const ProductReviewAddComponent = () => {
             <div className="w-1/2 flex flex-col items-center">
                 <img src="/images/pr_mario.png"
                     className="w-4/5 h-4/5 object-contain"/>
-                <h2 className="mt-5 text-2xl font-extrabold">상품명 : 슈퍼마리오</h2>
+                <h2 className="mt-5 text-2xl font-extrabold">&lt;상품명&gt; {productData.productTitle}</h2>
             </div>
             <div className="w-1/2 flex flex-col items-center">
                 <h3>
@@ -57,34 +119,44 @@ const ProductReviewAddComponent = () => {
                 </h3>
                 <div>
                     <fieldset id="rate">
-                        <input type="radio" id="rating10" name="rating" value="10" />
+                        <input type="radio" id="rating10" name="rating" value="10"
+                        onChange={handleChangeReview} />
                         <label for="rating10" title="5점"></label>
                         
-                        <input type="radio" id="rating9" name="rating" value="9" />
+                        <input type="radio" id="rating9" name="rating" value="9" 
+                        onChange={handleChangeReview} />
                         <label class="half" for="rating9" title="4.5점"></label>
                         
-                        <input type="radio" id="rating8" name="rating" value="8" />
+                        <input type="radio" id="rating8" name="rating" value="8" 
+                        onChange={handleChangeReview} />
                         <label for="rating8" title="4점"></label>
                         
-                        <input type="radio" id="rating7" name="rating" value="7" />
+                        <input type="radio" id="rating7" name="rating" value="7" 
+                        onChange={handleChangeReview} />
                         <label class="half" for="rating7" title="3.5점"></label>
                         
-                        <input type="radio" id="rating6" name="rating" value="6" />
+                        <input type="radio" id="rating6" name="rating" value="6" 
+                        onChange={handleChangeReview} />
                         <label for="rating6" title="3점"></label>
                         
-                        <input type="radio" id="rating5" name="rating" value="5" />
+                        <input type="radio" id="rating5" name="rating" value="5" 
+                        onChange={handleChangeReview} />
                         <label class="half" for="rating5" title="2.5점"></label>
                         
-                        <input type="radio" id="rating4" name="rating" value="4" />
+                        <input type="radio" id="rating4" name="rating" value="4" 
+                        onChange={handleChangeReview} />
                         <label for="rating4" title="2점"></label>
                         
-                        <input type="radio" id="rating3" name="rating" value="3" />
+                        <input type="radio" id="rating3" name="rating" value="3" 
+                        onChange={handleChangeReview} />
                         <label class="half" for="rating3" title="1.5점"></label>
                         
-                        <input type="radio" id="rating2" name="rating" value="2" />
+                        <input type="radio" id="rating2" name="rating" value="2" 
+                        onChange={handleChangeReview} />
                         <label for="rating2" title="1점"></label>
                         
-                        <input type="radio" id="rating1" name="rating" value="1" />
+                        <input type="radio" id="rating1" name="rating" value="1" 
+                        onChange={handleChangeReview} />
                         <label class="half" for="rating1" title="0.5점"></label>
                     </fieldset>
                 </div>
@@ -93,7 +165,9 @@ const ProductReviewAddComponent = () => {
                     자세한 평가를 적어주세요
                 </h3>
                 <textarea className="w-3/4 h-52 border p-4 m-4 resize-none"
-                    maxLength={300}/>
+                    maxLength={300}
+                    name="reviewContent"
+                    onChange={handleChangeReview}/>
 
                 <div className="flex w-3/4">
                     {previewUrl ? (
@@ -110,7 +184,8 @@ const ProductReviewAddComponent = () => {
                 </div>
                 
                 <div className="w-3/4 flex justify-end">
-                    <button className="text-white bg-[rgb(68,107,216)] text-2xl m-3 w-32 h-12 rounded-xl hover:opacity-90">
+                    <button className="text-white bg-[rgb(68,107,216)] text-2xl m-3 w-32 h-12 rounded-xl hover:opacity-90"
+                        onClick={handleClickSubmit}>
                         등록
                     </button>
                 </div>
