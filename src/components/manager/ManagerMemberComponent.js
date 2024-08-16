f621d9bf32e14965a3e140407b93d5f66e7e3dfd
SUH(home) <suh240505@gmail.com>
2024-08-17 00:09:48 +0900

    회원관리 table로 생성

--- src/components/manager/ManagerMemberComponent.js
+++ src/components/manager/ManagerMemberComponent.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getMemberList } from "../../api/memberApi";
+import styles from "../../css/memberPage.module.css"
 
 const initData =     {
     id: 0,
@@ -26,13 +27,29 @@ const ManagerMemberComponent = () => {
     },[]);
 
     return(
-        <div>
+        // ▼이거 class는 그냥 빨간선때문에 한거임 나중에 알아서 바꾸세욤 ㅎㅁㅎ)>
+        <div className={styles.test}> 
+            <table>
+                <tr>
+                    <th>회원번호</th>
+                    <th>회원 아이디</th>
+                    <th>회원 닉네임</th>
+                    <th>회원 등급</th>
+                    <th>회원 활동 상태</th>
+                </tr>
             {serverData.map(data=>
+            <>
 
-                <div>
-                    {data.id}, {data.memberId}, {data.memberName}, {data.memberNickname}, {data.memberRole}, {data.active}
-                </div>
+                <tr>
+                    <td>{data.id}</td>
+                    <td>{data.memberName}</td>
+                    <td>{data.memberNickname}</td>
+                    <td>{data.memberRole}</td>
+                    <td>{data.active==0? "활동" : "활동정지"}</td>
+                </tr>
+            </>
             )}
+            </table>
         </div>
     );
 
