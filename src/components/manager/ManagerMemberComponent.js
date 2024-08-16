ec266019974efb0794fbd4b8be9e05e5e3cd415c
SUH(home) <suh240505@gmail.com>
2024-08-17 01:57:26 +0900

    회원관리 회원등급 변경 설정

--- src/components/manager/ManagerMemberComponent.js
+++ src/components/manager/ManagerMemberComponent.js
@@ -1,20 +1,24 @@
 import { useEffect, useState } from "react";
 import { getMemberList } from "../../api/memberApi";
+import axios from "axios";
+
+// 빨간선때문에 import함 나중에 삭제하시길 ㅎㅎ
 import styles from "../../css/memberPage.module.css"
 
-const initData =     {
-    id: 0,
-    memberId: "",
-    memberName: "",
-    memberNickname: "",
-    memberRole: "",
-    active: false
-}
+// const initData =     {
+//     id: 0,
+//     memberId: "",
+//     memberName: "",
+//     memberNickname: "",
+//     memberSignupDate : "",
+//     memberRole: "",
+//     active: false
+// }
 
 
 const ManagerMemberComponent = () => {
 
-    const [serverData, setServerData] = useState([initData]);
+    const [serverData, setServerData] = useState([]);
 
     useEffect(()=>{
 
@@ -24,34 +28,60 @@ const ManagerMemberComponent = () => {
             console.log(data);
         }).catch(e=>console.log(e));
 
-    },[]);
+    },[serverData]);
+
+    // 회원등급 버튼 눌렀을 때, User<->Admin 됨
+    // serverData 자체를 변경해야됨
+    const clickMemberRole = (data) => {
+
+        const newRole = data.memberRole === "User" ? "Admin" : "User";
+
+        axios.post("http://localhost:8080/api/member/mypage/manager/change/role",{
+            id : data.id,
+            memberRole : newRole,
+        })
+        .then((response)=>{
+            console.log(response.data);
+        })
+        .catch((error)=>{
+            console.log("에러메세지 : " + error);
+        })
+    }
 
     return(
         // ▼이거 class는 그냥 빨간선때문에 한거임 나중에 알아서 바꾸세욤 ㅎㅁㅎ)>
         <div className={styles.test}> 
-            <table>
-                <tr>
-                    <th>회원번호</th>
-                    <th>회원 아이디</th>
-                    <th>회원 닉네임</th>
-                    <th>회원 등급</th>
-                    <th>회원 가입 날짜</th>
-                    <th>회원 활동 상태</th>
-                </tr>
-            {serverData.map(data=>
-            <>
-
-                <tr>
-                    <td>{data.id}</td>
-                    <td>{data.memberName}</td>
-                    <td>{data.memberNickname}</td>
-                    <td>{data.memberRole}</td>
-                    <td>{data.memberSignupDate}</td>
-                    <td>{data.active==0? "활동" : "활동정지"}</td>
-                </tr>
-            </>
-            )}
-            </table>
+            <div>총 회원 수 : {serverData.length}</div>
+            <div className={styles.membersGrid}>
+                <div className={styles.membersHeader}>
+                    <div>회원번호</div>
+                    <div>회원 아이디</div>
+                    <div>회원 닉네임</div>
+                    <div>회원 등급</div>
+                    <div>회원 가입 날짜</div>
+                    <div>회원 활동 상태</div>
+                </div>
+                {serverData.map(data => (
+                    <div className={styles.memberRow} key={data.id}>
+                        <div>{data.id}</div>
+                        <div>{data.memberName}</div>
+                        <div>{data.memberNickname}</div>
+                        <div>
+                            {data.memberRole}
+                            <button onClick={()=>clickMemberRole(data)}>
+                                {data.memberRole === "User" ? "🔄️Admin" : "🔄️User"}
+                            </button>
+                        </div>
+                        <div>{data.memberSignupDate}</div>
+                        <div>
+                            {data.active == 0 ? "활동" : "활동정지"}
+                            <button>
+                                {data.active == 0 ? "🔄️활동정지" : "🔄️활동"}
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 
