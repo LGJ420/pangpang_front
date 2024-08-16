5c349dd37ac279ac1f44e9aed851cb54a75dd660
SUH(home) <suh240505@gmail.com>
2024-08-16 23:06:55 +0900

    마이페이지-관리자 레이아웃 변경

--- src/pages/manager/ManagerIndexPage.js
+++ src/pages/manager/ManagerIndexPage.js
@@ -1,19 +1,19 @@
 import { Outlet } from "react-router-dom";
-import BasicWidthLimitLayout from "../../layouts/BasicWidthLimitLayout";
+import BasicNoHeightLayout from "../../layouts/BasicNoHeightLayout";
 import MypageLayoutComponent from "../../components/mypage/MypageLayoutComponent";
 
 const ManagerIndexPage = () => {
 
     return (
         
-        <BasicWidthLimitLayout>
+        <BasicNoHeightLayout>
 
             <MypageLayoutComponent>
                 <Outlet/>
             </MypageLayoutComponent>
     
 
-        </BasicWidthLimitLayout>
+        </BasicNoHeightLayout>
     );
 }
 
