d263ffe47175a10a475071c44c856b04733daed2
NSB(green) <suh240505@gmail.com>
2024-08-20 16:03:43 +0900

    마이페이지 프로필사진 반영 추가(주석 확인부탁)

--- src/components/mypage/MypageLayoutComponent.js
+++ src/components/mypage/MypageLayoutComponent.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import useCustomToken from "../../hooks/useCustomToken";
 import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const MypageLayoutComponent = ({children}) => {
 
@@ -8,7 +9,11 @@ const MypageLayoutComponent = ({children}) => {
 
     const navigate = useNavigate();
 
+    const [imageUrl, setImageUrl] = useState('/images/profile.png');
+
     useEffect(()=>{
+        console.log("decode token : " +decodeToken);
+        console.log('User ID:', decodeToken.id);
 
         // useEffect에도 이렇게 두번에 걸쳐 작업하는이유는
         // 리액트의 비동기 특성때문
@@ -16,18 +21,41 @@ const MypageLayoutComponent = ({children}) => {
 
             alert("잘못된 접근 방식입니다.");
             navigate(-1);
+            return; // 로그인하지 않은 경우, 나머지 코드 실행 방지
         }
-    },[isLogin]);
+
+        // ?? 왜 한번에 decodeToken.id 못찾는지 아는사람 (아래는 콘솔로그)
+            // decode token : [object Object]
+            // User ID:      여기 토큰.id들어가야된다능↓
+            // http://localhost:8080/api/member/view//image
+            // decode token : [object Object]
+            // User ID: 39 (29행은 id를 못찾은거라능!!!)
+            // http://localhost:8080/api/member/view/39/image
+        console.log(`http://localhost:8080/api/member/view/${decodeToken.id}/image`);
+
+        // 위 문제때문에 decodeToken.id 있을때만 실행시킴 고졸사토루 능지이슈 ㅈㅅ
+        if(decodeToken.id){
+            axios.get(`http://localhost:8080/api/member/view/${decodeToken.id}/image`)
+            .then(response => {
+                // 성공적으로 응답을 받았을 때 처리할 내용
+                console.log(response.data);
+                setImageUrl(`http://localhost:8080/api/member/view/${response.data}`);
+            })
+            .catch(error => {
+                // 오류가 발생했을 때 처리할 내용
+                console.error('서버에서 이미지 다운받기 실패:', error);
+            });
+        }
+    },[isLogin, decodeToken]);
 
     // 프로필사진
-    const [profileImage, setProfileImage] = useState("/images/profile.png")
+    // const [profileImage, setProfileImage] = useState("/images/profile.png")
 
-    useEffect(() => {
-        if (decodeToken.memberImage) {
-            setProfileImage(`http://localhost:8080/api/member/view/${decodeToken.memberImage}`);
-        }
-    }, [decodeToken.memberImage]);
-
+    // useEffect(() => {
+    //     if (decodeToken.memberImage) {
+    //         setProfileImage(`http://localhost:8080/api/member/view/${decodeToken.memberImage}`);
+    //     }
+    // }, [decodeToken.memberImage]);
 
     // 이 조건문은 반드시 리액트 훅보다
     // 그렇지 않으면 이조건이 통과되야 리액트가 발생하는 오류가 생겨
@@ -41,7 +69,7 @@ const MypageLayoutComponent = ({children}) => {
         <>
         <section>
             <div className="h-[15rem] flex items-center border-b bg-slate-100">
-                <img src={profileImage} className="object-cover rounded-full border w-52 h-52 ml-8"/>
+                <img src={imageUrl} className="object-cover rounded-full border w-52 h-52 ml-8"/>
                 <h3 className="text-5xl font-bold p-5 tracking-wider">{decodeToken.memberNickname}</h3>
                 <div className="text-2xl font-semibold pt-6 uppercase">등급 : {decodeToken.memberRole}</div>
             </div>
