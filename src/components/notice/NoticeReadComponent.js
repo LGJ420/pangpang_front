064039bbe52dbadbf945f7105a4572e3b44b91c4
SUH(home) <suh240505@gmail.com>
2024-08-27 22:09:53 +0900

    기정 공지사항 401->로그아웃->로그인 유도

--- src/components/notice/NoticeReadComponent.js
+++ src/components/notice/NoticeReadComponent.js
@@ -5,6 +5,7 @@ import { deleteNoticeComment, getNoticeComments, postNoticeComment, putNoticeCom
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { formatDateTime } from "../../util/dateUtil";
 import useCustomToken from "../../hooks/useCustomToken";
+import { logout } from '../../hooks/logout';
 
 
 const initNoticeData = {
@@ -133,7 +134,15 @@ const NoticeReadComponent = ({id}) => {
                     alert("삭제가 완료되었습니다.");
                     navigate(`/notice`, {replace: true});
                 })
-                .catch(error=>console.log(error));
+                .catch(error=>{
+                    console.log(error)
+                
+                    if (error.response.status === 401) {
+                        alert("토큰 유효 시간이 만료되었습니다.")
+                        logout(); // import { logout } from '../../hooks/logout'; 추가 필요
+                    }
+
+                });
         }
         
     }
