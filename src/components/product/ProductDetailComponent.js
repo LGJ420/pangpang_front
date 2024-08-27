dcf3bb2d75c5d22d4a2a99cae1693f1b1f9c69b0
chomin <doaiso381@naver.com>
2024-08-27 22:34:09 +0900

    리뷰 - 사용자 이미지 미등록시 기본 이미지 출력

--- src/components/product/ProductDetailComponent.js
+++ src/components/product/ProductDetailComponent.js
@@ -381,7 +381,7 @@ const ProductDetailComponent = ({ num }) => {
                   <div className="flex items-center">
                     <img
                       className="w-10 h-10 mr-2 rounded-full border"
-                      src={`${prefix}/${review.memberProfile}`} />
+                      src={ review.memberProfile != null ? `${prefix}/${review.memberProfile}` : '/images/profile.png'} />
                     <div className="mr-2">
                       {review.memberNickName}
                     </div>
