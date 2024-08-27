2db711ef8dc04103048bf3ee21ce505b56229a53
KIM <KIM>
2024-08-27 10:50:22 +0900

    날짜 형식 수정

--- src/util/dateUtil.js
+++ src/util/dateUtil.js
@@ -1,8 +1,13 @@
 // 날짜를 yyyy. mm. dd. 오전/오후 hh:mm 형식으로 바꿔줌
 export const formatDateTime = (dateTime) => {
-
     const date = new Date(dateTime);
 
-    return `${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`; // 초 단위 제거
+    const formattedDate = date.toLocaleDateString('en-GB'); // Use 'en-GB' for DD/MM/YYYY format
+    const formattedTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
+    // Convert DD/MM/YYYY to YYYY.MM.DD
+    const [day, month, year] = formattedDate.split('/');
+    const formattedDateString = `${year}.${month}.${day}`;
+
+    return `${formattedDateString} ${formattedTime}`;
 };
