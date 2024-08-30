3ef6098dfe5b2132be8a321e4c001efb79417cf1
NSB(green) <suh240505@gmail.com>
2024-08-30 16:31:57 +0900

    소셜로그인 인가 코드 받기

--- src/components/member/social_login/GoogleLoginBtn.js
+++ src/components/member/social_login/GoogleLoginBtn.js
@@ -1,14 +1,6 @@
 export default function GoogleLoginBtn() {
 
     const handleLogin = () => {
-    //     const clientId = process.env.GOOGLE_CLIENT_ID;
-    //     const redirectUri = window.location.href;
-    //     const scope = 'https://www.googleapis.com/auth/userinfo.profile';
-
-    //     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=${redirectUri}&client_id=${clientId}`
-
-    //     window.location.href = authUrl;
-
         // console.log('클라이언트 ID:', process.env.REACT_APP_GOOGLE_CLIENT_ID);   // 확인용
         // console.log('리디렉션 URL:', process.env.REACT_APP_GOOGLE_REDIRECT_URL);  // 확인용
 
